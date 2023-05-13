c4c7eb69ddb99e65d5f5471b468ae4f8903846ce
Saul Torres <carloscogake@gmail.com>
2023-05-13 11:52:31 -0600

    Cambios de aparincia y agregados de alertify services

--- src/app/clientes/alta-cliente/alta-cliente.component.ts
+++ src/app/clientes/alta-cliente/alta-cliente.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ClientesService } from '../clientes.service';
 import { Cliente, Grupo } from './../cliente.model';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AlertifyService } from 'src/app/service/aletify.service';
 
 @Component({
   selector: 'app-alta-cliente',
@@ -11,7 +12,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class AltaClienteComponent implements OnInit {
   cliente!: Cliente;
   grupos!: Grupo[];
-
+  ban:boolean=false;
   formularioAlta = new FormGroup({
     nombre: new FormControl('', [Validators.required, Validators.minLength(3)]),
     apellido: new FormControl('', [Validators.required, Validators.minLength(4)]),
@@ -21,17 +22,32 @@ export class AltaClienteComponent implements OnInit {
     mail: new FormControl('', [Validators.required, Validators.email])
   });
 
-  constructor(private clientesService: ClientesService) { }
+  constructor(private clientesService: ClientesService,public alertifyconfim:AlertifyService,public alertifyservice:AlertifyService) { 
+  }
 
   ngOnInit() {
     this.cliente = this.clientesService.nuevoCliente();
     this.grupos = this.clientesService.getGrupos();
   }
-
   nuevoCliente(): void {
     if (this.formularioAlta.valid) {
       this.clientesService.agregarCliente(this.cliente);
       this.cliente = this.clientesService.nuevoCliente();
-    } 
+      this.ban=true;      
+    }
+    if(this.ban==false){
+      this.alerr("Faltan validar campos");
+    }
+    if(this.ban==true){
+      this.alsuc("Reservacion realizada");
+      this.formularioAlta.disable();
+      
+    }
+  }
+  alerr(mesnaje:string){
+    this.alertifyservice.error(mesnaje);
+  }
+  alsuc(mesnaje:string){
+    this.alertifyservice.success(mesnaje)
   }
 }
