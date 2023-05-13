fa932461f753c207ce119fdf457a6bff7f23eb17
Saul Torres <carloscogake@gmail.com>
2023-05-13 12:31:31 -0600

    Funcionamiento principal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListadoClientesComponent } from './clientes/listado-clientes/listado-clientes.component';
 import { AltaClienteComponent } from './clientes/alta-cliente/alta-cliente.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { AcercaComponent } from './acerca/acerca.component';
 
-const routes: Routes = [  
+const routes: Routes = [
+{path:"inicio",component:InicioComponent},  
 {path:"lista",component:ListadoClientesComponent},
-{path:"alta",component:AltaClienteComponent}];
-
+{path:"alta",component:AltaClienteComponent},
+{path:"acerca",component:AcercaComponent},
+{path:"**",component:InicioComponent}
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,30 +9,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
-<<<<<<< HEAD
 import { ClientesModule } from './clientes/clientes.module';
-
-
-=======
-import { MostrarComponent } from './mostrar/mostrar.component';
-import { CapturarComponent } from './capturar/capturar.component';
 import { AcercaComponent } from './acerca/acerca.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { NavbarComponent } from './navbar/navbar.component';
->>>>>>> main
 
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-=======
-    MostrarComponent,
-    CapturarComponent,
     AcercaComponent,
     InicioComponent,
     NavbarComponent
->>>>>>> main
   ],
   imports: [
     BrowserModule,
