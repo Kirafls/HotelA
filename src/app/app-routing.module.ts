716caccdce808654d1444ecc07b57d357ebc8969
Saul Torres <carloscogake@gmail.com>
2023-05-13 12:18:54 -0600

    menu funcional de clientes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListadoClientesComponent } from './clientes/listado-clientes/listado-clientes.component';
+import { AltaClienteComponent } from './clientes/alta-cliente/alta-cliente.component';
 
 
 const routes: Routes = [
-  
+  {path:"lista",component:ListadoClientesComponent},
+  {path:"alta",component:AltaClienteComponent}
 ];
 
 @NgModule({
